d54f5f4c56c355ee302ece0f41a5c6091657a4fc
bvgr003 <148019599+bvgr003@users.noreply.github.com>
2024-03-21 18:58:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+
+var scrollToTopBtn = document.getElementById("scrollToTop");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+scrollToTopBtn.onclick = function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+};
