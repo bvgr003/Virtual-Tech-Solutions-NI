cd9148d423627dd44a4a24c0184fe8ca09b4a504
bvgr003 <148019599+bvgr003@users.noreply.github.com>
2024-03-23 07:45:32 -0600

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,71 @@
+var chatContainer = document.querySelector('.chat-container');
+var chatBox = document.getElementById('chat-box');
+var botName = 'Kapa';
+
+function toggleChatbot() {
+    chatContainer.style.display = chatContainer.style.display === 'none' ? 'block' : 'none';
+}
+
+
+function sendMessage() {
+    var userInput = document.getElementById('user-input').value;
+    if (userInput.trim() === '') {
+        return;
+    }
+    addMessage(userInput, 'user');
+    var botResponse = getBotResponse(userInput);
+    addMessage(botResponse, 'bot');
+    document.getElementById('user-input').value = '';
+    // Remove the toggleChatbot() call here
+}
+
+function addMessage(message, sender) {
+    var messageElement = document.createElement('div');
+    messageElement.classList.add('message', sender);
+    messageElement.innerText = message;
+    chatBox.appendChild(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+}
+
+function getBotResponse(userInput) {
+    userInput = userInput.toLowerCase().trim();
+    var response = '';
+
+    // Check for specific phrases or sentences
+    if (userInput.includes('work times') || userInput.includes('work hours') || userInput.includes('hours of operation')) {
+        response = "Our hours of operation are Monday to Fridays, 10am to 6pm and Saturdays 10am to 2pm Central Time.";
+    } else if (userInput.includes('pricing') || 
+               userInput.includes('prices') || 
+               userInput.includes('cost')) {
+        response = "For pricing information, please send an email to callassist18@gmail.com.";
+    } else if (userInput.includes('location') ||
+               userInput.includes('located') ||
+               userInput.includes('address')) {
+        response = "Our office is located at Enitel 1 C.N. 1/2 C.E. M/I, Ticuantepe, Nicaragua.";
+    } else if (userInput.includes('contact info') || userInput.includes('contact information') ||
+               userInput.includes('contact') ||
+               userInput.includes('phone number')) {
+        response = "You can contact us at callassist18@gmail.com or call us at 619-693-6911.";
+    } else if (userInput.includes('services') || userInput.includes('products') || userInput.includes('services')) {
+        response = "Our services offered are Virtual Assistance, Customer Support, Cold/Warm Calling, App Development, Website Development and Chatbot Development";
+    } else if (userInput.includes('company') || userInput.includes('company') ||
+        userInput.includes('business')) {
+        response = "Virtual Tech Solutions NI provides a comprehensive suite of services to meet your digital needs. Our services offered include virtual assistance, customer support, data entry, cold and warm calling, graphics design, app and website development. With our diverse range of services, we are committed to helping you succeed in today's competitive digital landscape.";
+    }
+    
+    // Initial message answer in no understanding
+    else {
+        response = "I'm sorry, I didn't understand that. Please ask about our company, work times, pricing, location, contact info or services offered.";
+    }
+
+    return response;
+}
+
+
+// Initial message
+addMessage(`Hello, I am ${botName}! How can I assist you today?`, 'bot');
+
+document.getElementById('chat-button').addEventListener('click', function () {
+    toggleChatbot();
+});
+
